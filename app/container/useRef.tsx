8e890fd64e187e6668a6287b5d08afce97decedf
jujube12 <gyubam1@gmail.com>
2024-05-23 00:59:40 +0900

    feat: useRef 사용해보기

--- app/container/useRef.tsx
+++ app/container/useRef.tsx
@@ -1,7 +1,19 @@
+'use client'
+import { useRef } from "react"
+
 export default function UseRef() {
+    const inputRef = useRef<null | HTMLInputElement>(null)
+
+    function btnClick() {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    }
     return (
         <div>
-            useRef 사용해보기
+            <div>useRef 사용해보기</div>
+            <input ref={inputRef}></input>
+            <button onClick={btnClick}>click</button>
         </div>
     )
 }
