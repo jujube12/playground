99fdb4fb6516a4b9df645c17cd54e19693d25681
jujube12 <gyubam1@gmail.com>
2024-05-29 01:08:47 +0900

    feat: [makeTitleImg] fontSize 조절 기능 구현

--- app/function/makeTitleImg/page.tsx
+++ app/function/makeTitleImg/page.tsx
@@ -1,52 +1,77 @@
-'use client'
-import { useEffect, useRef, useState } from "react"
-import style from '../function.module.css'
+"use client";
+import { useEffect, useRef, useState } from "react";
+import style from "../function.module.css";
+
+function ControlCanvas() {}
 
 export default function MakeTitleImg() {
-    const [text, setText] = useState('')
-    const canvasRef = useRef(null)
-    const dpr = window.devicePixelRatio
-    let inputText = ''
-    useEffect(()=>{
-        if(canvasRef.current){
-            let canvas:HTMLCanvasElement = canvasRef.current
-            let context = canvas.getContext('2d')
+    let inputText = "";
+
+    const canvasRef = useRef(null);
+    const dpr = window.devicePixelRatio;
+    let canvas: HTMLCanvasElement;
+    let context: CanvasRenderingContext2D | null;
+    let imgFontSize = 30;
+    function writeText(input: string, size: number) {
+        if (context) {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.textAlign = "center";
+            context.textBaseline = "middle";
+            context.font = size + "px selif";
+            context?.fillText(input, canvas.width / 2, canvas.height / 2);
+            // context.scale(dpr, dpr);
+        }
+    }
+    useEffect(() => {
+        //canvas 초기화
+        if (canvasRef.current) {
+            canvas = canvasRef.current;
+            context = canvas.getContext("2d");
             canvas.style.width = `500px`;
             canvas.style.height = `300px`;
-            canvas.width = 500 * dpr
-            canvas.height = 300 * dpr
-            if (context){
-                context.clearRect(0,0, canvas.width, canvas.height)
-                context.textAlign ='center'
-                context?.fillText(text, canvas.width/2, canvas.height/2)   
-                context.scale(dpr,dpr)
-            }
+            canvas.width = 500 * dpr;
+            canvas.height = 300 * dpr;
         }
-    })
+    }, []);
 
     return (
         <div className={style.makeTitleImg_wrapper}>
             <div className={style.makeTitleImg_title}>썸네일 만들기</div>
             <div className={style.makeTitleImg_input}>
-                <input onChange={(e)=>{inputText = e.target.value}}></input>
-                <button onClick={()=>{setText(inputText)}}>입력</button>
+                <input
+                    onChange={(e) => {
+                        inputText = e.target.value;
+                    }}></input>
+                <button
+                    onClick={() => {
+                        writeText(inputText, imgFontSize);
+                    }}>
+                    입력
+                </button>
             </div>
             <div className={style.makeTitleImg_canvas}>
-                <canvas ref={canvasRef} style={{backgroundColor:'white'}}></canvas>
+                <canvas ref={canvasRef} style={{ backgroundColor: "white" }}></canvas>
             </div>
             <div className={style.makeTitleImg_btn}>
                 <div className={style.makeTitleImg_changeBtn}>
                     <div>font size</div>
-                    <div>+</div>
-                    <div>-</div>
-                </div>
-                <div className={style.makeTitleImg_changeBtn}>
-                    <div>font position</div>
-                    <div>+</div>
-                    <div>-</div>
+                    <div
+                        onClick={() => {
+                            imgFontSize += 5;
+                            writeText(inputText, imgFontSize);
+                        }}>
+                        +
+                    </div>
+                    <div
+                        onClick={() => {
+                            imgFontSize -= 5;
+                            writeText(inputText, imgFontSize);
+                        }}>
+                        -
+                    </div>
                 </div>
                 <div>copy</div>
             </div>
         </div>
-    )
+    );
 }
